542f4c8f5024f89503a1520c969ce3df23a9e87a
Eryn Kelsey-Adkins <ekelseya@msudenver.edu>
2019-09-12 17:47:28 -0600

    Many to many cars to parts

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,6 @@
 class Part < ActiveRecord::Base
   belongs_to :factory
   # foreign key - factory_id
+  has_many :car_parts
+  has_many :cars, through: :car_parts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get '/pages/:page' => 'pages#show'
+  resources :car_parts
   resources :cars
   resources :parts
   resources :factories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get '/pages/:page' => 'pages#show'
 end
