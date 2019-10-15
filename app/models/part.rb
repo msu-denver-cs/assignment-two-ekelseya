bc3a5ac6d3b9c29bdf0e7c5286d14a4aacf2377c
Eryn Kelsey-Adkins <ekelseya@msudenver.edu>
2019-10-15 15:01:38 -0600

    Rejoined join table

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,8 +1,9 @@
 class Part < ActiveRecord::Base
   belongs_to :factory
-  # foreign key - factory_id
-  has_many :car_parts
-  has_many :cars, through: :car_parts
+
+  has_many :cars_parts, dependent: :delete_all
+  has_many :cars, through: :cars_parts
+
   validates :name, presence: true
   validates :quantity, presence: true
   validates :factory_id, presence: true
