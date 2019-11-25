f5452c58284cdf3a0ba5ad6f3d08059251b0c578
Eryn Kelsey-Adkins <ekelseya@msudenver.edu>
2019-11-25 07:58:03 -0700

    Removed search autocomplete
    Removed autocomplete for search in order to simplify SPA example

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@
 Rails.application.routes.draw do
 #  devise_for :users
   resources :car_parts
-  resources :cars do
-    get :autocomplete_car_make, on: :collection
-    collection do
-      get 'search'
-    end
-  end
+  resources :cars 
   resources :parts
   resources :factories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
